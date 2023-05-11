4a61e1811795856c0d245ff4161f7066b7b165e8
OmidSargazi <om.sargazi@gmail.com>
2023-05-12 00:35:28 +0330

    Build Navbar and install sass

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Link } from 'react-router-dom';
+import { FavoriteBorderOutlined, PersonOutlineOutlined, Search, ShoppingCartOutlined } from '@mui/icons-material';
+import "./Navbar.scss"
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className='wrapper'>
+      <div className='left'>
+       <div className='item'>
+        <img src='/images/en.png' alt='image'/>
+        <ExpandMoreIcon/>
+       </div>
+       <div className='item'>
+        <span>USD</span>
+        <ExpandMoreIcon/>
+       </div>
+       <div className='item'>
+        <Link className='link' to="/products/1">Woman</Link>
+        <Link className='link' to="/products/2">Man</Link>
+        <Link className='link' to="/products/3">Children</Link>
+       </div>
+      </div>
+      <div className='center'>
+        <Link className='link' to="/">Omid E-Commerce</Link>
+      </div>
+      <div className='right'>
+        <Link className='link'>HomePage</Link>
+        <Link className='link'>About Page</Link>
+        <Link className='link'>Contact</Link>
+        <Link className='link'>Stores</Link>
+        <div className='icons'>
+          <Search/>
+          <PersonOutlineOutlined/>
+          <FavoriteBorderOutlined/>
+          <div className='cartIcon'>
+            <ShoppingCartOutlined/>
+            <span>0</span>
+          </div>
+        </div>
+      </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
