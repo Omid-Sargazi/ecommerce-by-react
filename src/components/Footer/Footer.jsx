9114673e6c2f173fee899d11971d772b965a0f58
OmidSargazi <om.sargazi@gmail.com>
2023-05-12 16:43:34 +0330

    build Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
-
+import "./Footer.scss"
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='footer'>
+      <div className='top'>
+        <div className='items'>
+          <h1>Categories</h1>
+          <span>Woman</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+         
+        </div>
+        <div className='items'>
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className='items'>
+          <h1>About</h1>
+          <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime eveniet aut suscipit aliquid sapiente neque. Dolor atque . Obcaecati magnam dolores ex ad.</span>
+        </div>
+        <div className='items'>
+          <h1>Contact</h1>
+          <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime eveniet aut suscipit aliquid sapiente neque. Dolor atque . Obcaecati magnam dolores ex ad.</span>
+        </div>
+      </div>
+      <div className='bottom'>
+        <div className='left'>
+          <span className='logo'>Omid E-Commerce</span>
+          <span className='copyRight'>© Remix SoftwarSargazi_Omid</span>
+        </div>
+        <div className='right'>
+          <img src='/images/payment.png'/>
+        </div>
+      </div>
     </div>
   )
 }
